77025e9ca8ea78035816ed5f1e6c0b6cfca1e377
Yuriy Dubinin <yuradubinin100@gmail.com>
2025-11-26 17:10:39 +0300

    Initial template

---
+++ README.md
@@ -0,0 +1,2 @@
+# report-trades-history
+Summarized trades performed by a selected trader group over a specified period. Includes operation ID, date, symbol, price, profit, and account details.
---
+++ src/PluginInterface.cpp
@@ -0,0 +1,81 @@
+#include "PluginInterface.hpp"
+
+#include <iomanip>
+
+extern "C" void AboutReport(rapidjson::Value& request,
+                            rapidjson::Value& response,
+                            rapidjson::Document::AllocatorType& allocator,
+                            CServerInterface* server) {
+    response.AddMember("version", 1, allocator);
+    response.AddMember("name", Value().SetString("Trades History report", allocator), allocator);
+    response.AddMember("description",
+    Value().SetString("Summarized trades performed by a selected trader group over a specified period. "
+                                 "Includes operation ID, date, symbol, price, profit, and account details.",
+             allocator), allocator);
+    response.AddMember("type", REPORT_RANGE_GROUP_TYPE, allocator);
+}
+
+extern "C" void DestroyReport() {}
+
+extern "C" void CreateReport(rapidjson::Value& request,
+                             rapidjson::Value& response,
+                             rapidjson::Document::AllocatorType& allocator,
+                             CServerInterface* server) {
+    // Структура накопления итогов
+    struct Total {
+        double volume;
+        double commission;
+        double profit;
+        std::string currency;
+    };
+
+    std::unordered_map<std::string, Total> totals_map;
+
+    std::string group_mask;
+    int from;
+    int to;
+    if (request.HasMember("group") && request["group"].IsString()) {
+        group_mask = request["group"].GetString();
+    }
+    if (request.HasMember("from") && request["from"].IsNumber()) {
+        from = request["from"].GetInt();
+    }
+    if (request.HasMember("to") && request["to"].IsNumber()) {
+        to = request["to"].GetInt();
+    }
+
+    std::vector<TradeRecord> trades_vector;
+    std::vector<GroupRecord> groups_vector;
+
+    try {
+        server->GetCloseTradesByGroup(group_mask, from, to, &trades_vector);
+        server->GetAllGroups(&groups_vector);
+    } catch (const std::exception& e) {
+        std::cerr << "[TradesHistoryReportInterface]: " << e.what() << std::endl;
+    }
+
+    // Лямбда для поиска валюты аккаунта по группе
+    auto get_group_currency = [&](const std::string& group_name) -> std::string {
+        for (const auto& group : groups_vector) {
+            if (group.group == group_name) {
+                return group.currency;
+            }
+        }
+        return "N/A"; // группа не найдена - валюта не определена
+    };
+
+    // Лямбда подготавливающая значения double для вставки в AST (округление до 2-х знаков)
+    auto format_for_AST = [](double value) -> std::string {
+        std::ostringstream oss;
+        oss << std::fixed << std::setprecision(2) << value;
+        return oss.str();
+    };
+
+
+    // Total report
+    const Node report = div({
+        h1({text("Trades History Report")})
+    });
+
+    utils::CreateUI(report, response, allocator);
+}
